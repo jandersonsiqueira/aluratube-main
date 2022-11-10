45ec29bbe4694ccced719043daf963c6904a9b6c
Janderson Siqueira <janderssampaio@gmail.com>
2022-11-10 20:38:18 -0300

    modified:   pages/video.js

--- pages/video.js
+++ pages/video.js
@@ -1,49 +1,14 @@
-import { useRouter } from "next/router";
-import React from "react";
-import styled from "styled-components";
-import Menu from "../../src/components/Menu";
-
-const StyledVideo = styled.div`
-  h2 {
-    font-size: 24px;
-    margin-bottom: 16px;
-    text-transform: capitalize;
-  }
-  section {
-    width: 100vw;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    margin-top: 100px;
-    padding: 16px;
-  }
-  iframe {
-    width: 100%;
-    max-width: 1080px;
-    height: 540px;
-  }
-`;
-
+import { useContext } from "react";
+import { ColorModeContext } from "../src/components/Menu/components/ColorMode";
 export default function Video() {
-  const router = useRouter();
-  const data = router.query;
+  const contexto = useContext(ColorModeContext);
   return (
-    <>
-      <Menu />
-      <StyledVideo>
-        <section>
-          <h2>{data.title}</h2>
-          <iframe
-            className="videoPlayer"
-            src={`https://www.youtube.com/embed/${data.url}`}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </section>
-      </StyledVideo>
-    </>
-  );
+    <div>
+        Vídeo!
+        {contexto.mode}
+        <button onClick={() => contexto.toggleMode()}>
+            Trocar modo
+        </button>
+    </div>
+)
 }
